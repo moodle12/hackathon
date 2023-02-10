9026ca7de4a62c9a78adc9356400a1be51f6bef6
moodle12 <103280407+moodle12@users.noreply.github.com>
2023-02-10 12:29:41 +0530

    round1 link commit

--- src/components/Teacher/Teacher_Dashboard/TDashboard.jsx
+++ src/components/Teacher/Teacher_Dashboard/TDashboard.jsx
@@ -25,17 +25,10 @@ const TDashboard = () => {
             Result Upload
           </div>
           <div className="tDashboard-item__2" onClick={assignmentHandle}>
-<<<<<<< HEAD
            Assignment Upload
           </div>
           <div className="tDashboard-item__3">
            <a className="text_style" rel="noreferrer" target="_blank" href="https://frolicking-macaron-a53493.netlify.app/"> Mark Attendance</a>
-=======
-            Mark Attendance
-          </div>
-          <div className="tDashboard-item__3" onClick={markAttendanceHandle}>
-            Assignment Upload
->>>>>>> 655762a2fde62d5a0c8fb38e5b58ec14c11791e7
            <a className="text_style" rel="noreferrer" target="_blank" href="https://stately-banoffee-e93132.netlify.app/"> Mark Attendance</a>
           </div>
         </div>
@@ -46,20 +39,12 @@ const TDashboard = () => {
           <div className="cards">
             <Card
               title="Physics-STD-9"
-<<<<<<< HEAD
               image="https://tse2.mm.bing.net/th?id=OIP.n6KSNUF9ZjqfsKv9VkQfbgHaHa&pid=Api&P=0"
-=======
-              image="https://images.theconversation.com/files/191827/original/file-20171025-25516-g7rtyl.jpg?ixlib=rb-1.1.0&rect=0%2C70%2C7875%2C5667&q=45&auto=format&w=926&fit=clip"
->>>>>>> 655762a2fde62d5a0c8fb38e5b58ec14c11791e7
               time="4:00 to 5:00"
             />
             <Card
               title="Chemistry-STD-7"
-<<<<<<< HEAD
               image="https://tse2.mm.bing.net/th?id=OIP.KQOBsn2-Nh1TJieIchFBnwHaHa&pid=Api&P=0"
-=======
-              image="https://images.theconversation.com/files/121885/original/image-20160510-20731-1pf8nwv.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop"
->>>>>>> 655762a2fde62d5a0c8fb38e5b58ec14c11791e7
               time="5:00 to 6:00"
             />
           </div>
