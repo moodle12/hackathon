2ce7786c9fa2fdd4f3e1861421c9407c69387150
Divy <divyparekh1810@gmail.com>
2023-02-10 10:30:29 +0530

    added teacher dashboard and change route

--- src/App.js
+++ src/App.js
@@ -22,33 +22,47 @@ import AddUser from "./components/admin/Users/AddUser";
 import PDashboard from "./components/parent/parent_dashboard/PDashboard";
 import IDashboard from "./components/Institution/Institute_Dashboard/IDashboard";
 import TDashboard from "./components/Teacher/Teacher_Dashboard/TDashboard";
+import ResultUpload from "./components/Teacher/Result-upload/ResultUpload";
 const App = () => {
-  var loc= window.location.pathname
+  var loc = window.location.pathname;
   return (
     <>
       <Router>
-      {loc === "/"? <NavigationBar/>:null } 
+        <NavigationBar />
         <Routes>
-        <Route path="/admin/manageusers"  element={<Users/>}/>
-          <Route path="/home" element={<HomePage/>} />
-          <Route path="/" element={<LogIn/>} />
+          <Route path="/admin/manageusers" element={<Users />} />
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LogIn />} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/payment" element={<Payment />} />
           <Route path="/admission" element={<Admission />} />
           <Route path="/community" element={<Community />} />
           <Route path="/eBooks" element={<EBook />} />
           <Route path="/guidance" element={<Guidance />} />
-          <Route path="/results" element={<Result/>}/>
-          <Route path="/marks" element={<Marks/>}/>
-          <Route path="/pdf" element={<Pdf/>}/>
-          <Route path="/admin/dashboard" exact element={<DefaultLayout/>}/>
-          <Route path="/admin/adduser" exact element={<AddUser/>}/>
-          <Route path="/parent/dashboard" exact element={<PDashboard/>}></Route>
-          <Route path="/institute/dashboard" exact element={<IDashboard/>}></Route>
-          <Route path="/teacher/dashboard" exact element={<TDashboard/>}></Route>
+          <Route path="/results" element={<Result />} />
+          <Route path="/marks" element={<Marks />} />
+          <Route path="/pdf" element={<Pdf />} />
+          <Route path="/resultUpload" element={<ResultUpload />} />
+          <Route path="/admin/dashboard" exact element={<DefaultLayout />} />
+          <Route path="/admin/adduser" exact element={<AddUser />} />
+          <Route
+            path="/parent/dashboard"
+            exact
+            element={<PDashboard />}
+          ></Route>
+          <Route
+            path="/institute/dashboard"
+            exact
+            element={<IDashboard />}
+          ></Route>
+          <Route
+            path="/teacher/dashboard"
+            exact
+            element={<TDashboard />}
+          ></Route>
         </Routes>
       </Router>
-      
+
       {/* <SignUp /> */}
     </>
   );
--- src/components/Teacher/Teacher_Dashboard/TDashboard.jsx
+++ src/components/Teacher/Teacher_Dashboard/TDashboard.jsx
@@ -1,28 +1,85 @@
-import React from 'react'
-import axios from "axios";
-import { useState, useEffect } from "react";
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import Card from "../../card/Card";
+import "./TDashboard.css";
 
 const TDashboard = () => {
-  const [data, setData] = useState([]);
-  useEffect(() => {
-    axios
-      .get("http://localhost:9109/upload").then(
-        (res) =>{ 
-          setData(res.data)
-        })
-      .catch((err) => console.log(err, "it has an error"));
-  });
-  return (
-    <div>
-      <h1>Teacher's Dashboard</h1>
-      {data.map((singleData,index) => {
-        const base64String = btoa(
-          String.fromCharCode(...new Uint8Array(singleData.image.data.data))
-        );
-        return <img key={index} src={`data:image/png;base64,${base64String}`} width="500" alt='nothing to display'/>
-      })}
-    </div>
-  )
-}
+  let navigate = useNavigate();
 
-export default TDashboard
+  const resultHandle = () => {
+    navigate("/resultUpload");
+  };
+
+  const assignmentHandle = () => {
+    navigate("/assignmentUpload");
+  };
+
+  const markAttendanceHandle = () => {
+    navigate("/markAttendanceUpload");
+  };
+  return (
+    <div className="tDashboard-container">
+      <div className="tDashboard-sidebar">
+        <div className="tDashboard-items">
+          <div className="tDashboard-item__1" onClick={resultHandle}>
+            Result Upload
+          </div>
+          <div className="tDashboard-item__2" onClick={assignmentHandle}>
+            Mark Attendance
+          </div>
+          <div className="tDashboard-item__3" onClick={markAttendanceHandle}>
+            Assignment Upload
+          </div>
+        </div>
+      </div>
+      <div className="tDashboard-main">
+        <div className="tDashboard-main__left">
+          <h2 className="schedule__title">22/12/2023 Schedule</h2>
+          <div className="cards">
+            <Card
+              title="Physics-STD-9"
+              image="https://images.theconversation.com/files/191827/original/file-20171025-25516-g7rtyl.jpg?ixlib=rb-1.1.0&rect=0%2C70%2C7875%2C5667&q=45&auto=format&w=926&fit=clip"
+              time="4:00 to 5:00"
+            />
+            <Card
+              title="Chemistry-STD-7"
+              image="https://images.theconversation.com/files/121885/original/image-20160510-20731-1pf8nwv.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=900.0&fit=crop"
+              time="5:00 to 6:00"
+            />
+          </div>
+        </div>
+        <div className="tDashboard-main__right">
+          <img
+            src="https://www.zohowebstatic.com/sites/zweb/images/calendar/perfect-business-calendar-2x.png"
+            alt="calendar"
+            style={{
+              marginTop: "1rem",
+              height: "200px",
+              width: "280px",
+              marginLeft: "2rem",
+            }}
+            className="tDashboard-main__right__image"
+          />
+          <span className="tDashboard-main__right__span">Recent Files:- </span>
+          <div className="tDashboard-items__right">
+            <div className="tDashboard-item__right__1">📁 STD-6_Maths.pdf</div>
+            <div className="tDashboard-item__right__2">
+              📁 STD-7_Science.pdf
+            </div>
+            <div className="tDashboard-item__right__3">
+              📁 STD-9_Physics.pdf
+            </div>
+            <div className="tDashboard-item__right__3">
+              📁 STD-5_Chemistry.pdf
+            </div>
+            <div className="tDashboard-item__right__3">
+              📁 STD-9_Physics.pdf
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TDashboard;
